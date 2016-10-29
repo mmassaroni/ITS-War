2c92e489e4e4d761045a1d62ed4d682b4d669c7f
gcobucci <gcobucci98@gmail.com>
2016-10-29 19:39:15 -0300

    no anda el fetch array

--- public_html/juego/fachada/fachada.php
+++ public_html/juego/fachada/fachada.php
@@ -8,11 +8,8 @@
 			$usuarioLocal->actualizar();
 		}
 		elseif ($_GET['accion']=="1") {
-			die("no");
 			$usuarioLocal->setestado("buscando");
-
 			$usuarioLocal->actualizar();
-
 			mostrarPersonajes($usuarioLocal);
 
 		}
@@ -35,14 +32,13 @@
 		$habilidades=array();
 		
 		$registrosPer = $db->query("select p.* from personaje p, usu_tiene_per utp, usuario u where u.id = utp.usuario and utp.personaje = p.id and u.id = ". $usuarioLocal->getid() .";") or die("ERROR CON LA BD");
-		echo "asd". $registrosPer['id'];
 
-		while($registroPer = mysqli_fetch_array($registrosPer)){
+		while($registroPer = mysqli_fetch_array($registrosPer, MYSQLI_ASSOC)){
 			header('location:index.html');
 			$objPer = new Personaje($registroPer["id"], $registroPer["nombre"], $registroPer["imgCuerpo"], $registroPer["imgFicha"], $registroPer["fuerza"], $registroPer["movimiento"], $registroPer["resistencia"], $registroPer["alcance"], $registroPer["vida"], $registroPer["energia"], $registroPer["precio"]);
 			$db2 = new Conexion();
 			$registrosHab = $db2->query("select h.* from habilidad h, personaje p where p.id = h.personaje and p.id = '". $objPer.getid() ."'") or die("ERROR CON LA BD2");
-			while($registroHab = mysqi_fetch_array($registrosHab)){
+			while($registroHab = mysqi_fetch_array($registrosHab, MYSQLI_ASSOC)){
 				
 			}
 
