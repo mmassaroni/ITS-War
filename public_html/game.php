e8663414f7f5fc6fced3879cd968a2570892544b
mmassaroni <miguelmassaroni@gmail.com>
2016-10-31 09:30:27 -0300

    btn atacar

--- public_html/game.php
+++ public_html/game.php
@@ -222,6 +222,33 @@
 			function mostrarTablero() {
 				myBtn.style.backgroundColor = "rgba(0,0,0,0)";
 			}
+
+			
+			// #################
+			//  BOTON DE ATAQUE
+			// #################
+
+
+			/* When the user clicks on the button,
+			toggle between hiding and showing the dropdown content */
+			function myFunction() {
+			    document.getElementById("myDropdown").classList.toggle("show");
+			}
+
+			// Close the dropdown if the user clicks outside of it
+			window.onclick = function(event) {
+			  if (!event.target.matches('.dropbtn')) {
+
+			    var dropdowns = document.getElementsByClassName("dropdown-content");
+			    var i;
+			    for (i = 0; i < dropdowns.length; i++) {
+			      var openDropdown = dropdowns[i];
+			      if (openDropdown.classList.contains('show')) {
+			        openDropdown.classList.remove('show');
+			      }
+			    }
+			  }
+			}
 		</script>
 	</body>
 </html>
