843d9f5801bb1ebaa1e42853b3823cc98a852321
mmassaroni <miguelmassaroni@gmail.com>
2016-11-04 17:28:19 -0300

    JS funcion ESPERANDO

--- public_html/game.php
+++ public_html/game.php
@@ -227,11 +227,26 @@
 			    }
 			  }
 			}
+
+
+
+<?php if ($_GET['accion'] == "esperando") { echo '
+// ##############################
+// #                            #
+// #  ESPERANDO - TIMER - AJAX  #
+// #                            #
+// ##############################
+
+		
+		setInterval(turno ,5000);									// repite la funcion "turno()" cada 5s
+		function turno(){
+			$.ajax({
+                url:   "/juego/fachada/fachada.php?caca=1",         // ejecuta la fachada con un valor de $_GET a definir para ejecutar una funcion
+        	});
+        	$("#panel1").load("juego/generador/panel_iz.php");		// "recarga" el panel de la iz
+        	$("#panel2").load("juego/generador/panel_d.php");		// "recarga" el panel de la de
+		}
+';}?>
 		</script>
 	</body>
 </html>
-<?php  
-if($_GET['accion']=="esperando") {
-	esperando();
-}
-?>
