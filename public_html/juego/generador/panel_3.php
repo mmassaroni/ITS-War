d2cfd1d0a38422340b19bb041371aaaad8e3fc13
mmassaroni <miguelmassaroni@gmail.com>
2016-11-17 14:03:54 -0300

    ta

--- public_html/juego/generador/panel_3.php
+++ public_html/juego/generador/panel_3.php
@@ -1,7 +1,12 @@
 <div class="movimientos">
-	
+	<a href="#" class="movBtn">MOVER</a>
+	<a href="#" class="movBtn">PASAR</a>
 </div>
 <hr>
 <div class="ataques">
+	<div class="tooltip"><a href="#" style="margin: 0 15px;"><img src="../../images/juego/at1.png" style="margin-top: 4px"><span class="tooltiptext">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, explicabo, consequatur. Laboriosam laudantium reiciendis unde praesentium, at atque delectus perspiciatis cupiditate hic saepe eum nostrum possimus iusto, ratione molestias quas.</span></a></div>
 	
+	<div class="tooltip"><a href="#" style="margin: 0 15px;"><img src="../../images/juego/at2.png" style="margin-top: 4px"><span class="tooltiptext">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, explicabo, consequatur. Laboriosam laudantium reiciendis unde praesentium, at atque delectus perspiciatis cupiditate hic saepe eum nostrum possimus iusto, ratione molestias quas.</span></a></div>
+	
+	<div class="tooltip"><a href="#" style="margin: 0 15px;"><img src="../../images/juego/at3.png" style="margin-top: 4px"><span class="tooltiptext">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum, explicabo, consequatur. Laboriosam laudantium reiciendis unde praesentium, at atque delectus perspiciatis cupiditate hic saepe eum nostrum possimus iusto, ratione molestias quas.</span></a></div>
 </div>
