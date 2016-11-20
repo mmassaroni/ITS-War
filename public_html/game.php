69e88c4c49bc7bba823473f8e62390e5492dc005
mmassaroni <miguelmassaroni@gmail.com>
2016-11-20 18:53:47 -0300

    Pero que correcion papa

--- public_html/game.php
+++ public_html/game.php
@@ -247,7 +247,7 @@
 		<div id="juego">	
 			<div class="row">
 				<div id="panel1" class="panel">
-					<?php if ($_GET['accion'] != "esperando" and $_GET['accion'] != "jugando"){echo "<img src='/images/logo.png' style='-webkit-animation-name: rotate; -webkit-animation-duration: 4s; -webkit-animation-iteration-count: infinite; animation-name: rotate; animation-duration: 4s; animation-iteration-count: infinite; width: 200px; margin-top: 45%;'>";}?>
+					<?php if ($_GET['accion'] != "esperando" and $_GET['accion'] != "jugando"){echo "<img src='/images/logo.png' style='width: 200px; margin-top: 45%;'>";}?>
 				</div><!--cierre panel1-->
 				
 				<div id="tablero" <?php if ($_GET['accion']=='jugando') { echo "style='text-align: left;'";	} ?>>
@@ -300,7 +300,7 @@
 				</div><!--cierre tablero-->
 				
 				<div id="panel2" class="panel">
-					<?php if ($_GET['accion'] != "esperando" and $_GET['accion'] != "jugando"){echo "<img src='/images/mrBean.gif' style='margin-top: 20%'><img src='/images/loading.gif' style='width: 105px;'>";}?>
+					<?php if ($_GET['accion'] != "esperando" and $_GET['accion'] != "jugando"){echo "<img src='/images/logo.png' style='width: 200px; margin-top: 45%;'>";}?>
 				</div><!--cierre panel2-->
 
 			</div>
@@ -308,7 +308,7 @@
 			<div class="row">
 				
 				<div id="panel3">
-					<?php if ($_GET['tab'] == null) {echo "<img src='/images/bob.gif' style='width:160px'>";}?>
+					<?php if ($_GET['tab'] == null) {echo "<img src='/images/mate.png' style='-webkit-animation-name: rotate; -webkit-animation-duration: 4s; -webkit-animation-iteration-count: infinite; animation-name: rotate; animation-duration: 4s; animation-iteration-count: infinite; width:160px'>";}?>
 				</div><!-- cierre panel3 -->
 
 			</div>
